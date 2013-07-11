6764e097a402e47d433c9274aa928e145a78a03f
Robert J. Hijmans <r.hijmans@gmail.com>
2013-07-11 19:48:53 +0000

    version 2.1-49

--- R/focalWeight.R
+++ R/focalWeight.R
@@ -43,7 +43,7 @@
 
 
 .rectangle.weight <- function(rs, d) {
-	d <- rep_len(d, length.out=2)
+	d <- rep(d, length.out=2)
 	nx <- 1 + 2 * floor(d[1]/rs[1])
 	ny <- 1 + 2 * floor(d[2]/rs[2])
 	m <- matrix(1, ncol=nx, nrow=ny)
